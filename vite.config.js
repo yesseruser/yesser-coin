34e5f01692930b4dfeb71cc5438ad5105d35b9a6
yesseruser <yesseruseryt@gmail.com>
2025-08-08 18:13:46 +0200

    Add migrate page

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         mint: resolve(__dirname, 'mint.html'),
+        migrate: resolve(__dirname, 'migrate.html')
       },
     },
   }
